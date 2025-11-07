ce56d3227fd91fedd3491ca2cbfcb35747cca3f6
KraljBarti <barti.kujundzic75@gmail.com>
2025-11-07 19:55:33 +0100

    add initial RentEASE frontend with full Next.js page structure

---
+++ frontend/app/my_rentals/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center p-10">
+      <h1 className="text-6xl font-extrabold tracking-tight">My Rentals</h1>
+    </main>
+  );
+}
---
+++ frontend/app/support&safety/insurance/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1 className="text-6xl font-extrabold tracking-tight">
+        Insurance Information
+      </h1>
+    </main>
+  );
+}
