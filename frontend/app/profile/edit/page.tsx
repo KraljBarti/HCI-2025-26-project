3ed994403acaded7a25990a60fd353ea96650c4f
barti_kujundzic <147097335+KraljBarti@users.noreply.github.com>
2025-12-21 18:28:34 +0100

    Delete frontend directory

--- frontend/app/profile/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-6xl font-extrabold tracking-tight">
-        Edit Profile / Settings
-      </h1>
-    </main>
-  );
-}
